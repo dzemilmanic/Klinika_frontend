4a55c67872eaf536f9b2fad14fb1d2dd428bd272
Džemil Manić <dzemilmanic@hotmail.com>
2024-11-26 09:29:24 +0100

    login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css"; // Importuj CSS datoteku
 
-const Login = () => {
-    return <h2>Ovo je stranica Login.</h2>;
-};
-
-export default Login;
+export default function Login() {
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <h2 className="login-title">Prijavi se</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Unesite vaš email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Lozinka</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Unesite vašu lozinku"
+            />
+          </div>
+          <button type="submit" className="login-button">
+            Prijavi se
+          </button>
+        </form>
+        <div class="register-link">
+          Nemate registrovan nalog? <a href="/register">Registrujte se</a>
+        </div>
+      </div>
+    </div>
+  );
+}
