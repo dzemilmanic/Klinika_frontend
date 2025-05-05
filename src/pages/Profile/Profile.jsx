7246685787e3365732b22ab7e273516e85459d75
Džemil Manić <dzemilmanic@hotmail.com>
2025-05-05 14:55:47 +0200

    profile update2

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
 import { Lock } from "lucide-react";
-import AllAppointmentsModal from "./AllAppointmentsModal";
-import MedicalRecordModal from "./MedicalRecordModal";
+import AllAppointmentsModal from "../../components/Profile/AllAppointmentsModal";
+import MedicalRecordModal from "../../components/Profile/MedicalRecordModal";
 import "./Profile.css";
 import { toast } from "react-toastify";
 
