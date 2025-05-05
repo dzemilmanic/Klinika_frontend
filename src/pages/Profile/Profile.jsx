7b5a7ea67a85ba55dad58d7c27487dcff0e2dd0b
Džemil Manić <dzemilmanic@hotmail.com>
2025-05-05 14:44:58 +0200

    profile update

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
 import { Lock } from "lucide-react";
-import AllAppointmentsModal from "../../components/Profile/AllAppointmentsModal";
-import MedicalRecordModal from "../../components/Profile/MedicalRecordModal";
+import AllAppointmentsModal from "./AllAppointmentsModal";
+import MedicalRecordModal from "./MedicalRecordModal";
 import "./Profile.css";
 import { toast } from "react-toastify";
 
@@ -357,10 +357,11 @@ export default function Profile() {
             {modalField === "biography" && (
               <div>
                 <label>Biografija:</label>
-                <input
-                  type="text"
+                <textarea
+                  className="biography-textarea"
                   value={newBiography}
                   onChange={(e) => setNewBiography(e.target.value)}
+                  placeholder="Unesite svoju biografiju..."
                 />
               </div>
             )}
